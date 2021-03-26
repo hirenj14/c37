3c009350132470ffabe2aa6a22d6be837ae10cc8
hirenj14 <76769318+hirenj14@users.noreply.github.com>
2021-03-26 16:01:12 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,41 @@
+class Game {
+  constructor(){}
+  
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+   
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+    if(gameState === 0){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+    }
+  }
+  play(){
+    form.hide();
+    textSize(30);
+    text("game start",120,100)
+    player.getPlayerInfo();
+
+   if(allPlayers !== undefined){
+     var display_position=130;
+     for (var plr in allPlayers){
+       if(plr==="players"+player.index){
+         fil("red");
+       }
+     }
+   }
+  }
+}
